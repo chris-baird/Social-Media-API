56a25e7630dfab208fd63510aaf264158df906f0
Chris Baird <cab3953@gmail.com>
2022-09-11 18:24:04 -0700

    User controller get single user
    Added a method in the user controller to get a single user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,10 +4,24 @@ class UserController {
   constructor(service) {
     this.service = service
     this.getUsers = this.getUsers.bind(this)
+    this.getSingleUser = this.getSingleUser.bind(this)
   }
 
   getUsers(req, res) {
     this.service.getUsers().then((dbUserData) => {
+      res.json(dbUserData);
+    })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
+
+  getSingleUser(req, res) {
+    this.service.getSingleUser().then((dbUserData) => {
+      if (!dbUserData) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
       res.json(dbUserData);
     })
       .catch((err) => {
