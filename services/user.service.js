16ae6d29af0e50ac74a6099b3856bad56f9d1b75
Chris Baird <cab3953@gmail.com>
2022-09-11 18:22:38 -0700

    User service get single user method
    Added a method to get a single user

--- services/user.service.js
+++ services/user.service.js
@@ -4,11 +4,19 @@ class UserService {
   constructor(models) {
     this.models = models
     this.getUsers = this.getUsers.bind(this)
+    this.gitSingleUser = this.gitSingleUser.bind(this)
   }
 
   getUsers() {
     return this.models.User.find()
       .select('-__v')
+  }
+
+  gitSingleUser(id) {
+    return this.models.User.findOne({ _id: id })
+      .select('-__v')
+      .populate('friends')
+      .populate('thoughts')
   }
 }
 
