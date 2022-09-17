3df62626cfdc04953255cd2f5a6864848c1023fd
Chris Baird <cab3953@gmail.com>
2022-09-17 14:33:07 -0700

    Added route for deleting a friend

--- routes/api/user.routes.js
+++ routes/api/user.routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend } = require("../../controllers/user.controller")
+const { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend, removeFriend } = require("../../controllers/user.controller")
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser)
 
-router.route('/:userId/friends/:friendId').post(addFriend)
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend)
 
 module.exports = router;
