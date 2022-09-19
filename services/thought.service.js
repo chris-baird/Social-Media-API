3c40eee748594dc2be178d6246288139acd08b4a
Chris Baird <cab3953@gmail.com>
2022-09-19 10:03:54 -0700

    Thought service get thoughts method
    Added a method that gets all the thoughts

--- services/thought.service.js
+++ services/thought.service.js
@@ -3,7 +3,13 @@ const { Thought } = require("../models")
 class ThoughtService {
   constructor(services) {
     this.services = services
+    this.getThoughts = this.getThoughts.bind(this)
     this.removeReaction = this.removeReaction.bind(this)
+  }
+
+  getThoughts() {
+    return Thought.find()
+      .sort({ createdAt: -1 })
   }
 
   removeReaction(dbUserData) {
